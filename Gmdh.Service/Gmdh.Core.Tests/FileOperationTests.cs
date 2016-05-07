de49062d5b4c37b44e1d11bf4bcba24b7ec46f04
IhorKorotenko <ihorkorotenko@gmail.com>
2016-05-07 17:50:13 +0300

    Project initialized. Added file read operations and basic tests for them

---
+++ Gmdh.Service/Gmdh.Core.Tests/FileOperationTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gmdh.Core.Tests
+{
+    [TestClass]
+    public class FileOperationTests
+    {
+        [TestMethod]
+        public void ReadExcelFile_XlsxExtension()
+        {
+            var currentDirectory = GetProjectFSPath();
+            var filePath = $@"{currentDirectory}/test.xlsx";
+            var data = FilesOperations.ReadExcelFile(filePath);
+            Assert.IsNotNull(data);
+        }
+
+        [TestMethod]
+        public void ReadValidExcelFile_XlsExtension_ShouldReturnNotEmptyData()
+        {
+            var currentDirectory = GetProjectFSPath();
+            var filePath = $@"{currentDirectory}/testOldFormat.xls";
+            var data = FilesOperations.ReadExcelFile(filePath);
+            Assert.IsNotNull(data);
+        }
+
+        [TestMethod]
+        public void ReadValidCsvFile_ShoulReturnNotEmptyData()
+        {
+            var currentDirectory = GetProjectFSPath();
+            var filePath = $@"{currentDirectory}/testCSV.csv";
+            var data = FilesOperations.ReadCsvFile(filePath);
+            Assert.IsNotNull(data);
+        }
+
+
+        [TestMethod]
+        public void ReadValidTextFile_ShouldReturnNotEmptyData()
+        {
+            var currentDirectory = GetProjectFSPath();
+            var filePath = $@"{currentDirectory}/test.txt";
+            var data = FilesOperations.ReadPlainTextFile(filePath);
+            Assert.IsNotNull(data);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException),"Smth wrong")]
+        public void ReadExcelFileWithInvalidFormatOfCells_ShouldThrowException()
+        {
+            var currentDirectory = GetProjectFSPath();
+            var filePath = $@"{currentDirectory}/invalid.xlsx";
+            FilesOperations.ReadExcelFile(filePath);
+        }
+
+
+        private static string GetSolutionFSPath()
+        {
+            return System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        }
+
+        private static string GetProjectFSPath()
+        {
+            return $"{GetSolutionFSPath()}\\{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}";
+        }
+    }
+}
---
+++ Gmdh.Service/Gmdh.Core/FilesOperations.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using CsvHelper;
+using Excel;
+
+
+namespace Gmdh.Core
+{
+    public static class FilesOperations
+    {
+        /// <summary>
+        /// Reads the excel file specified in fileName param. We assume that data in file is stored like matrix, and there is only one worksheet in the specified file
+        /// </summary>
+        /// <param name="filePath">Full path to excel file to read</param>
+        /// <returns></returns>
+        public static List<List<double>> ReadExcelFile(string filePath)
+        {
+            var resultList = new List<List<double>>();
+
+            using (IExcelDataReader excelReader = GetExcelReader(filePath))
+            {
+                var dataSet = excelReader.AsDataSet();
+                var worksheet = dataSet.Tables[0];
+                var rows = GetRows(worksheet);
+                resultList.AddRange(
+                    rows.Select(
+                        dataRow => dataRow.ItemArray.Cast<double>().ToList()
+                        )
+                    );
+
+                return resultList;
+            }
+        }
+
+        /// <summary>
+        /// Reads the csv file with sample data matrix in it.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="separator"></param>
+        /// <returns>List of lists of int(two dimensional matrix)</returns>
+        public static List<List<double>> ReadCsvFile(string filePath,char separator=';')
+        {
+
+            using (var fileStream = new FileStream(filePath,FileMode.Open,FileAccess.Read))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    var resultList = new List<List<double>>();
+                    while (!streamReader.EndOfStream)
+                    {
+                        var row = streamReader.ReadLine().Split(separator);
+                        var doubleList = row.Select(double.Parse);
+                        resultList.Add(doubleList.ToList());
+                    }
+                    return resultList;
+                }
+            }
+        }
+
+        public static List<List<double>> ReadPlainTextFile(string filePath,char separator=' ')
+        {
+            using (var fileStream = new FileStream(filePath,FileMode.Open,FileAccess.Read))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    var resultList = new List<List<double>>();
+                    while (!streamReader.EndOfStream)
+                    {
+                        var row = streamReader.ReadLine().Split(separator);
+                        var doubles = row.Select(double.Parse);
+                        resultList.Add(doubles.ToList());
+                    }
+                    return resultList;
+                }
+            }
+        } 
+
+        private static IExcelDataReader GetExcelReader(string filePath,bool isfirstRowsAsColumnNames=false)
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var fileExtension = GetExtesion(filePath);
+                IExcelDataReader reader;
+                switch (fileExtension)
+                {
+                    case ".xls":
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                        break;
+                    }
+                    case ".xlsx":
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                        break;
+                    }
+                    default:
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(fileExtension));
+                    }
+                }
+                reader.IsFirstRowAsColumnNames = isfirstRowsAsColumnNames;
+                return reader;
+            }
+        }
+
+        private static IEnumerable<DataRow> GetRows(DataTable table)
+        {
+            var resultSet = from DataRow row in table.Rows
+                            select row;
+            return resultSet;
+        }
+        private static string GetExtesion(string filePath)
+        {
+            return Path.GetExtension(filePath);
+        } 
+    }
+}
